85546009ababfbb228a7a0519bd985b40a232f46
LeGro <sibiar2016@gmail.com>
2024-03-08 01:42:11 +0300

    hw transformations fixes

--- Assets/HW Transformations/Scripts/Mover.cs
+++ Assets/HW Transformations/Scripts/Mover.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeRotating : MonoBehaviour
+public class Mover : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
     void Update()
     {
-        Rotate();
-    }
-
-    private void Rotate()
-    {
-        transform.Rotate(new Vector3(0, _speed, 0));
+        transform.position += transform.forward * _speed * Time.deltaTime;
     }
 }
---
+++ Assets/HW Transformations/Scripts/Scaler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Scaler : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    void Update()
+    {
+        transform.localScale += Vector3.one * _speed * Time.deltaTime;
+    }
+}
